adc73f19efb99269610685fe11baf86ce23b2b64
leidian977 <49711842+leidian977@users.noreply.github.com>
2019-08-27 20:00:34 +0800

    Update run_classifier.py

--- run_classifier.py
+++ run_classifier.py
@@ -192,7 +192,7 @@ class DataProcessor(object):
 class AProcessor(DataProcessor):
     def get_train_examples(self, data_dir):
         # 读取数据集
-        file_path = os.path.join(data_dir, 'comments_train.csv')
+        file_path = os.path.join(data_dir, 'train.csv')
         df = pd.read_csv(file_path)
         # 划分训练集和测试集
         df_train, self.df_test = train_test_split(df, test_size=0.2)
